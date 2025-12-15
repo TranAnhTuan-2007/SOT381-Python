3f0c45e3f086f47235be9c2f3775deda0ead98cf
SevenCreator <tuan.ta.67khmt@ntu.edu.vn>
2025-12-15 13:56:09 +0700

    Create nhap_va_xuat_hcn.py

---
+++ nhap_va_xuat_hcn.py
@@ -0,0 +1,5 @@
+dai =int(input("Nhập chiều dài:"))
+rong =int(input("Nhập chiều rộng:"))
+chu_vi_hcn = dai + rong
+dien_tich_hcn = dai * rong
+print(f"chu vi của hình chữ nhật là {chu_vi_hcn} và diện tích của hcn là {dien_tich_hcn}")
