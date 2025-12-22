a3b619108e094724df9b9f775b731db6040377f2
Trần Anh Tuấn <tuan.ta.67khmt@ntu.edu.vn>
2025-12-22 14:36:12 +0700

    thực hành

--- chu_vi_dien_tich_hcn.py
+++ chu_vi_dien_tich_hcn.py
@@ -1,11 +1,9 @@
-import math
-a=float(input("nhập cạnh tam giác a: "))
-b=float(input("nhập cạnh tam giác b: "))
-c=float(input("nhập cạnh tam giác c: "))
-if a+b>c and b+c>a and a+c>b:
-    chu_vi=a + b + c
-    p= chu_vi/2
-    dien_tich= math.sqrt(p*(p-a)*(p-b)*(p-c))
+w=float(input("nhập chiều rộng: "))
+h=float(input("nhập chiều cao: "))
+if 0 <= h <= 100 and 0 <= w <= 100:
+    chu_vi=(w+h)*2
+    dien_tich=w*h
     print(f"chu vi hình chữ nhật là {chu_vi:.2f}")
     print(f"diện tích hình chữ nhật là {dien_tich:.2f}")
-    
+else:
+    print("giá trị không nằm trong khoảng 0 đến 100")
